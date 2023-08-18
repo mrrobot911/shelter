97bb2ea2bc4cef68600fdbc5822bb6c164c2cdc0
mrrobot911 <kelwin2705@gmail.com>
2023-08-18 23:45:23 +0300

    feat: add burger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,8 +8,12 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   flag = true;
+  burger = false;
   constructor(private router: Router) { }
   ngOnInit() {
     this.flag = this.router.url === '/pets';
+  }
+  clickBurger() {
+    this.burger = !this.burger;
   }
 }
